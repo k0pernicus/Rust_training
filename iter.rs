aaef484c9a49b272f250055a725b5af9a94dc9f7
Antonin Carette <antonin.carette@gmail.com>
2015-07-09 16:26:15 +0200

    iter.rs : new examples of iteration

--- iter.rs
+++ iter.rs
@@ -1,4 +1,4 @@
-fn main() {
+fn simple_iter() {
     let mut iter = 1..10;
 
     loop {
@@ -10,4 +10,32 @@ fn main() {
 
     println!("The End!");
 
+}
+
+fn simple_consumer() {
+    let cons = (1..10).collect::<Vec<u32>>();
+
+    for i in cons {
+        println!("{}", i);
+    }
+}
+
+fn find_in_consumers() {
+    let cons = (1..1001).find(|x| *x > 22);
+
+    for i in cons {
+        println!("{}", i);
+    }
+}
+
+fn filter_in_consumers() {
+    let cons = (1..1001).filter(|x| *x % 2 == 0);
+
+    for i in cons {
+        println!("{}", i);
+    }
+}
+
+fn main() {
+    filter_in_consumers();
 }
